c2fe653d4f2213566f9bd6705e5b109d2e3c0e37
Zeph <zephaniahsefuoduori@gmail.com>
2023-03-12 21:02:08 +0300

    Add emoji helper

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,11 @@ dependencies {
     implementation("androidx.compose.material:material:1.2.0")
     implementation ("com.google.accompanist:accompanist-pager:0.29.1-alpha")
     implementation ("androidx.compose.ui:ui-util:1.3.3")
+    val emoji2_version = "1.2.0"
+
+    implementation("androidx.emoji2:emoji2:$emoji2_version")
+    implementation("androidx.emoji2:emoji2-views:$emoji2_version")
+    implementation("androidx.emoji2:emoji2-views-helper:$emoji2_version")
 
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     implementation ("app.rive:rive-android:4.0.0")
--- app/src/main/java/io/sefu/nuntium/presentation/authentication/LoginScreen.kt
+++ app/src/main/java/io/sefu/nuntium/presentation/authentication/LoginScreen.kt
@@ -1,6 +1,10 @@
 package io.sefu.nuntium.presentation.authentication
 
+import android.widget.ImageView
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.emoji2.widget.EmojiTextView
 import androidx.navigation.NavController
 import io.sefu.nuntium.composable.HeaderDescriptionTopBar
 
@@ -10,6 +14,10 @@ fun LoginScreen(navController: NavController) {
         header = "Welcome Back \uD83D\uDC4B",
         description = "I am happy to see you again. You can continue where you left off by logging in"
     ) {
-
+        AndroidView(factory = { context ->
+            EmojiTextView(context).apply {
+                text = "\uD83D\uDE00"
+            }
+        })
     }
 }
