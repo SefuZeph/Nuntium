0d89bd82f96be7bbee03d789de1fd702aeb52717
Zeph <zephaniahsefuoduori@gmail.com>
2023-03-30 19:09:48 +0300

    Initial topic selection

--- app/src/main/java/io/sefu/nuntium/composable/NuntiumTopicCard.kt
+++ app/src/main/java/io/sefu/nuntium/composable/NuntiumTopicCard.kt
@@ -1,7 +1,9 @@
 package io.sefu.nuntium.composable
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -9,13 +11,15 @@ import androidx.compose.ui.unit.dp
 import io.sefu.nuntium.designsystem.NuntiumTheme
 
 @Composable
-fun NuntiumTopicCard(topicName: String,modifier: Modifier) {
+fun NuntiumTopicCard(topicName: String, modifier: Modifier, isSelected: Boolean) {
     Box(
         modifier = modifier
             .height(72.dp)
             .fillMaxWidth()
-            .background(color = NuntiumTheme.colors.greyLighter, shape = NuntiumTheme.shapes.medium),
-        contentAlignment = Alignment.Center
+            .background(
+                color = if (isSelected) NuntiumTheme.colors.primaryPurple else NuntiumTheme.colors.greyLighter,
+                shape = NuntiumTheme.shapes.medium
+            ), contentAlignment = Alignment.Center
     ) {
         NuntiumText(text = topicName)
     }
--- app/src/main/java/io/sefu/nuntium/presentation/topics/TopicScreen.kt
+++ app/src/main/java/io/sefu/nuntium/presentation/topics/TopicScreen.kt
@@ -1,11 +1,14 @@
 package io.sefu.nuntium.presentation.topics
 
+import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -15,32 +18,50 @@ import io.sefu.nuntium.composable.NuntiumButton
 import io.sefu.nuntium.composable.NuntiumTopicCard
 import io.sefu.nuntium.navigation.Screens
 
+data class TopicSelection(val index: Int, val value: String,val isSelected:Boolean,)
+
 @Composable
 fun TopicScreen(navController: NavController) {
-    val topicList = listOf(
-        "\uD83C\uDFC8  Sports",
-        "⚖️ Politics",
-        "\uD83C\uDF1E  Life",
-        "\uD83C\uDFAE  Gaming",
-        "\uD83D\uDC3B  Animals",
-        "\uD83C\uDF34  Nature",
-        "\uD83C\uDF54  Food",
-        "\uD83C\uDFA8  Art",
-        "\uD83D\uDCDC  History",
-        "\uD83D\uDC57  Fashion"
 
-
+    val topicList = mutableListOf(
+        TopicSelection(0, "\uD83C\uDFC8  Sports",false),
+        TopicSelection(1, "⚖️ Politics",false),
+        TopicSelection(2, "\uD83C\uDF1E  Life",false),
+        TopicSelection(3, "\uD83C\uDFAE  Gaming",false),
+        TopicSelection(4, "\uD83D\uDC3B  Animals",false),
+        TopicSelection(5, "\uD83C\uDF34  Nature",false),
+        TopicSelection(6, "\uD83C\uDF54  Food",false),
+        TopicSelection(7, "\uD83C\uDFA8  Art",false),
+        TopicSelection(8, "\uD83D\uDCDC  History",false),
+        TopicSelection(9, "\uD83D\uDC57  Fashion",false)
     )
+    val selectedItems = remember { mutableSetOf<Int>() }
+
+    fun toggleSelection(index: Int) {
+        if (selectedItems.contains(index)) {
+            selectedItems.remove(index)
+        } else {
+            selectedItems.add(index)
+        }
+    }
+
     HeaderDescriptionTopBar(
         header = "Select your favorite topics",
         description = "Select some of your favorite topics to let us suggest better news for you."
     ) {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
+        LazyVerticalGrid(columns = GridCells.Fixed(2),
             contentPadding = PaddingValues(top = 32.dp),
             content = {
                 items(topicList.size) {
-                    NuntiumTopicCard(topicName = topicList[it], modifier = Modifier.padding(4.dp))
+                    NuntiumTopicCard(
+                        topicName = topicList[it].value,
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .clickable {
+                                toggleSelection(topicList[it].index)
+                            },
+                        isSelected = selectedItems.contains(it)
+                    )
                 }
             })
 
